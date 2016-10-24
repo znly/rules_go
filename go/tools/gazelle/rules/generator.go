002fd0e4fb0f07851c8bc187b6e638d44d46b86f
Paul Bethe <pmbethe09@users.noreply.github.com>
2016-10-24 09:37:16 -0400

    Revert "perform the main sandwich for go_binary" (#169)

--- go/tools/gazelle/rules/generator.go
+++ go/tools/gazelle/rules/generator.go
@@ -91,20 +91,11 @@ func (g *generator) Generate(rel string, pkg *build.Package) ([]*bzl.Rule, error
 		rules = append(rules, p)
 	}
 
-	libName := defaultLibName
-	r, err := g.generateLib(rel, libName, pkg)
+	r, err := g.generate(rel, pkg)
 	if err != nil {
 		return nil, err
 	}
 	rules = append(rules, r)
-
-	if pkg.IsCommand() {
-		r, err := g.generateBin(rel, libName, pkg)
-		if err != nil {
-			return nil, err
-		}
-		rules = append(rules, r)
-	}
 
 	p, err := g.filegroup(rel, pkg)
 	if err != nil {
@@ -132,31 +123,20 @@ func (g *generator) Generate(rel string, pkg *build.Package) ([]*bzl.Rule, error
 	return rules, nil
 }
 
-func (g *generator) generateBin(rel, library string, pkg *build.Package) (*bzl.Rule, error) {
-	kind := "go_binary"
-	name := path.Base(pkg.Dir)
-
-	visibility := checkInternalVisibility(rel, "//visibility:public")
-	attrs := []keyvalue{
-		{key: "name", value: name},
-		{key: "library", value: ":" + library},
-		{key: "visibility", value: []string{visibility}},
-		{key: "deps", value: []string{library}},
-	}
-
-	return newRule(kind, nil, attrs)
-
-}
-
-func (g *generator) generateLib(rel, name string, pkg *build.Package) (*bzl.Rule, error) {
+func (g *generator) generate(rel string, pkg *build.Package) (*bzl.Rule, error) {
 	kind := "go_library"
+	name := defaultLibName
+	if pkg.IsCommand() {
+		kind = "go_binary"
+		name = path.Base(pkg.Dir)
+	}
 
 	visibility := "//visibility:public"
-	// Libraries made for a go_binary should not be exposed to the public.
-	if pkg.IsCommand() {
-		visibility = "//visibility:private"
+	if i := strings.LastIndex(rel, "/internal/"); i >= 0 {
+		visibility = fmt.Sprintf("//%s:__subpackages__", rel[:i])
+	} else if strings.HasPrefix(rel, "internal/") {
+		visibility = "//:__subpackages__"
 	}
-	visibility = checkInternalVisibility(rel, visibility)
 
 	attrs := []keyvalue{
 		{key: "name", value: name},
@@ -173,17 +153,6 @@ func (g *generator) generateLib(rel, name string, pkg *build.Package) (*bzl.Rule
 	}
 
 	return newRule(kind, nil, attrs)
-}
-
-// checkInternalVisibility overrides the given visibility if the package is
-// internal.
-func checkInternalVisibility(rel, visibility string) string {
-	if i := strings.LastIndex(rel, "/internal/"); i >= 0 {
-		visibility = fmt.Sprintf("//%s:__subpackages__", rel[:i])
-	} else if strings.HasPrefix(rel, "internal/") {
-		visibility = "//:__subpackages__"
-	}
-	return visibility
 }
 
 // filegroup is a small hack for directories with pre-generated .pb.go files
