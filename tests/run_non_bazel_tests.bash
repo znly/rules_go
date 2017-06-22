6562954d860adcd2aa85ebf02f871af17eb86253
Ian Cottrell <ian.the.hat@gmail.com>
2017-06-22 18:29:25 -0400

    Migrate test_filter_test_1.7.5 (#571)
    Migrate from bash test to bazel test.
    This test is marked dev (not on ci) for now to match previous behaviour, not
    sure why we don't want to run it by default though?

--- tests/run_non_bazel_tests.bash
+++ tests/run_non_bazel_tests.bash
@@ -24,7 +24,6 @@ tests=(
 manual_tests=(
   build_gazelle_with_1.7/build_gazelle_with_1.7.bash
   custom_go_toolchain/custom_go_toolchain.bash
-  test_filter_test_1.7.5/test_filter_test_1.7.5.bash
 )
 if [ "$1" != "ci" ]; then
   tests+=("${manual_tests[@]}")
