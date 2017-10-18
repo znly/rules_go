039d5830e50a8fddfd1888666be7f147e4b3c961
Ian Cottrell <ian.the.hat@gmail.com>
2017-10-18 09:16:04 -0400

    Write the go_info rule in terms of a builder (#931)
    This means it reports identical information to the tools that actually get run
    during a normal build.

---
+++ go/private/rules/info.bzl
@@ -0,0 +1,57 @@
+# Copyright 2014 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+load("@io_bazel_rules_go//go/private:actions/action.bzl",
+    "action_with_go_env",
+)
+
+def _go_info_script_impl(ctx):
+  go_toolchain = ctx.toolchains["@io_bazel_rules_go//go:toolchain"]
+  out = ctx.actions.declare_file(ctx.label.name+".bash")
+  action_with_go_env(ctx, go_toolchain,
+      inputs = [],
+      outputs = [out],
+      mnemonic = "GoInfo",
+      executable = ctx.executable._go_info,
+      arguments = ["-script", "-out", out.path],
+  )
+  return [
+      DefaultInfo(
+          files = depset([out]),
+      ),
+  ]
+
+_go_info_script = rule(
+    _go_info_script_impl,
+    attrs = {
+      "_go_info": attr.label(
+          allow_files = True,
+          single_file = True,
+          executable = True,
+          cfg = "host",
+          default="@io_bazel_rules_go//go/tools/builders:info")
+    },
+    toolchains = ["@io_bazel_rules_go//go:toolchain"],
+)
+
+def go_info():
+  _go_info_script(
+      name = "go_info_script",
+      tags = ["manual"],
+  )
+  native.sh_binary(
+      name = "go_info",
+      srcs = ["go_info_script"],
+      tags = ["manual"],
+  )
---
+++ go/tools/builders/info.go
@@ -0,0 +1,82 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// info prints debugging information about the go environment.
+// It is used to help examine the execution environment of rules_go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+const endOfHereDoc = "EndOfGoInfoReport"
+
+func invoke(goenv *GoEnv, out *os.File, args []string) error {
+	cmd := exec.Command(goenv.Go, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = append(os.Environ(), goenv.Env()...)
+	if out != nil {
+		cmd.Stdout = out
+		cmd.Stderr = out
+	}
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error running %v: %v", args, err)
+	}
+	return nil
+}
+
+func run(args []string) error {
+	filename := ""
+	script := false
+	flags := flag.NewFlagSet("info", flag.ExitOnError)
+	flags.BoolVar(&script, "script", script, "Write in script mode")
+	flags.StringVar(&filename, "out", filename, "The file to write the report to")
+	goenv := envFlags(flags)
+	if err := flags.Parse(args); err != nil {
+		return err
+	}
+	f := os.Stderr
+	if filename != "" {
+		var err error
+		f, err = os.Create(filename)
+		if err != nil {
+			return fmt.Errorf("Could not create report file: %v", err)
+		}
+		defer f.Close()
+	}
+	if script {
+		fmt.Fprintln(f, "cat << "+endOfHereDoc)
+	}
+	if err := invoke(goenv, f, []string{"version"}); err != nil {
+		return err
+	}
+	if err := invoke(goenv, f, []string{"env"}); err != nil {
+		return err
+	}
+	if script {
+		fmt.Fprintln(f, endOfHereDoc)
+	}
+	return nil
+}
+
+func main() {
+	if err := run(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
+}
