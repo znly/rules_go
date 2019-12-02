3efdc83f1a89ba219af6a5c9f612440f4eabca8f
Jay Conrod <jayconrod@google.com>
2019-12-02 17:08:50 -0500

    tests/legacy/trans_dep_error: delete test (#2303)
    This is the bazel_test. It checks whether a package can be imported as
    both a direct and transitive dependency.
    
    This test provides little value and is not worth migrating to
    go_bazel_test, so deleting.
    
    Updates #2285

--- tests/legacy/trans_dep_error/a.go
+++
@@ -1,3 +0,0 @@
-package a
-
-import _ "github.com/bazelbuild/rules_go/tests/trans_dep_error/b"
--- tests/legacy/trans_dep_error/lib.go
+++
@@ -1,4 +0,0 @@
-package trans_dep_error
-
-import _ "github.com/bazelbuild/rules_go/tests/trans_dep_error/a"
-import _ "github.com/bazelbuild/rules_go/tests/trans_dep_error/b"
