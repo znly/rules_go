9833a4f07f6a08fc2e56cb1adec31c474d0c14f3
Steeve Morin <steeve@zen.ly>
2021-04-05 21:55:12 +0200

    go/tools/builders: add special case for framework flag in LDFLAGS_ALLOW
    The -framework flag needs to have its value in the same or clause.
    
    This fixed boostraping stdlib on iOS.
    
    Signed-off-by: Steeve Morin <steeve@zen.ly>

--- go/tools/builders/stdlib.go
+++ go/tools/builders/stdlib.go
@@ -101,6 +101,11 @@ You may need to use the flags --cpu=x64_windows --compiler=mingw-gcc.`)
 		b.WriteString(sep)
 		sep = "|"
 		b.WriteString(regexp.QuoteMeta(f))
+		// If the flag if -framework, the flag value needs to be in the same
+		// condition.
+		if f == "-framework" {
+			sep = " "
+		}
 	}
 	os.Setenv("CGO_LDFLAGS_ALLOW", b.String())
 
